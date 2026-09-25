14f0d31fb1bfa6c0ac131fe2ddb59cefa5160328
ci <example@example.com>
2026-09-26 02:35:09 +0800

    fix

--- lib/pages/setting/widgets/bv_jump_dialog.dart
+++ lib/pages/setting/widgets/bv_jump_dialog.dart
@@ -43,7 +43,7 @@ Future<void> showBvSwitchDialog(BuildContext context) async {
 
                     SetSwitchItem(
                       title: '每次启动时跳转',
-                      subtitle: '启动时将忽略上次的BV号/链接' + (Pref.jumpDirec ? '直接跳转': '并显示跳转选项'),
+                      subtitle: '启动时将忽略上次的BV号/链接' + (Pref.jumpDirec ? '然后直接跳转': '并显示跳转选项'),
                       setKey: SettingBoxKey.shouldJumpEveryTime,
                       defaultVal: false,
                       contentPadding: EdgeInsets.symmetric(horizontal: 24),
